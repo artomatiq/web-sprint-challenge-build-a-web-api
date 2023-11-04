3ab45144ae8ec652f82e5a0f714212b5694874e0
Art Vardanyan <akvardanyan99@gmail.com>
2023-11-04 13:06:01 -0400

    added error 500 middleware

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -3,4 +3,11 @@ const Actions = require('./actions-model')
 
 const router = express.Router();
 
+router.use((error, req, res, next)=>{
+    res.status(error. status || 500).json({
+        message: error.message || 'error in the Projects router',
+        stack: error.sstack
+    })
+})
+
 module.exports = router;
